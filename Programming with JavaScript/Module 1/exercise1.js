97d858bdcca8b248d46452accf7cc83e91616b05
andretokumoto <andretokumoto@gmail.com>
2025-01-13 12:14:55 -0300

    exercise advanced use of operators

---
+++ Programming with JavaScript/Module 1/exercise1.js
@@ -0,0 +1,12 @@
+var score = 8
+
+console.log("Mid-level skills:"+score)
+
+var timeRemaining = 0
+var energy = 10
+
+console.log("Game over:"+ (timeRemaining == 0 || energy == 0))
+
+var num = 9
+
+console.log("o numero "+num+" eh par? "+(num%2==0))
